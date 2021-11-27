1b5b0c77a1dde35a68c53685aca9b7d6fc7cec8e
kyawzinmyat <kyawzinmyat00000001@gmail.com>
2021-11-27 20:58:30 +0630

    Initial Commit

--- DjangoGit/settings.py
+++ DjangoGit/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Test',
+    'StudentTeacherManagement',
+    'Courses',
+    'UserManagement',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'DjangoGit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- DjangoGit/urls.py
+++ DjangoGit/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from Test.views import test_view,google
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",test_view),
+    path("google",google),
+    path("signup",include('UserManagement.urls'))
 ]
